d53d4bd2f18c63fcc76056207b8c7a3c0315ec62
Drew Lair <dlair@nd.edu>
2023-08-30 13:20:57 -0400

    Initial Tests Pass for Encoder

--- encoder.c
+++ encoder.c
@@ -15,10 +15,26 @@ int string_decode( const char *es, char *s ){
             continue;
         }
 
+        else if (*curr == '\''){
+
+            decoded_str[ decoded_i++ ] = *(++curr);
+            if ( *(++curr) != '\'' || *(curr+1) != '\0'){
+                printf("Error: Bad character string\n");
+                return 1;
+            }
+            decoded_str[ decoded_i ] = '\0';
+
+            int len = sizeof(decoded_str);
+            strncpy(s, decoded_str, len);
+            printf("succcess\n");
+            return 0;
+        }
+
         else if( *curr != '\"' ){
             printf("Bad string, must start with a double-quote\n");
             return 1;
         }
+
 
         else{
             curr++;
@@ -26,10 +42,30 @@ int string_decode( const char *es, char *s ){
 
                 if ( *curr == '\"' ){
 
-                    if ( decoded_i < size && !(*(curr+1)) ){
-                        decoded_str[ decoded_i ] = '\0';
-                        s = decoded_str;
+                    if ( decoded_i < size ){
+                        if (*(curr+1) != '\0' && isspace( *(curr+1) ) ){
+                            decoded_str[ decoded_i++ ] = '\0';
+                        }
+                        curr++;
+                        while( decoded_i++ < size && isspace( *(curr) ) ){
+                            curr++;
+                        }
+
+                        if (decoded_i >= MAX_STRING_LEN){
+                            printf("Error: String is too long!\n");
+                            return 1;
+                        }
+                        else if ( *curr != '\0'){
+                            printf("Bad String: Text after end quote!\n");
+                            printf("%c\n", *curr);
+                            return 1;
+                        }
+
+                        int len = sizeof(decoded_str);
+                        strncpy(s, decoded_str, len);
+                        printf("success: %s\n", decoded_str);
                         return 0;
+
 
                     }
                     else{
@@ -39,21 +75,82 @@ int string_decode( const char *es, char *s ){
                     }
 
                 }
+
                 else if ( *curr == '\\' ){
                     curr++;
-                    if ( *curr == 'n' ||
-                         *curr == 't' ||
-                         *curr == 'v' ||
-                         *curr == 'f' ||
-                         *curr == 'r' 
-                       ){
 
-                        decoded_str[ decoded_i++ ] = '\\';
+                    if (*curr == 'a'){
+                        decoded_str[ decoded_i++ ] = '\a';
+                    }
+                    else if (*curr == 'b'){
+                        decoded_str[ decoded_i++ ] = '\b';
+                    }
+                    else if (*curr == 'e'){
+                        decoded_str[ decoded_i++ ] = '\e';
+                    }
+                    else if (*curr == 'f'){
+                        decoded_str[ decoded_i++ ] = '\f';
+                    }
+                    else if ( *curr == 'n' ){
+                        decoded_str[ decoded_i++ ] = '\n';
+                    }
+                    else if (*curr == 'r'){
+                        decoded_str[ decoded_i++ ] = '\r';
+                    }
+                    else if (*curr == 't'){
+                        decoded_str[ decoded_i++ ] = '\t';
+                    }
+                    else if (*curr == 'v'){
+                        decoded_str[ decoded_i++ ] = '\v';
+                    }
+
+                    else if ( *curr == '\"' || *curr == '\\' || *curr == '\''){
                         decoded_str[ decoded_i++ ] = *curr;
                     }
 
-                    else if ( *curr == '\"' ){
-                        decoded_str[ decoded_i++ ] = *curr;
+                    else if (*curr == '0' && *(curr+1) == 'x'){
+
+                        //Add hex 0x and check for correct hexvals after
+                        decoded_str[ decoded_i++ ] = *(curr++);
+                        decoded_str[ decoded_i++ ] = *(curr++);
+
+                        if (
+                            !( isdigit(*curr)) &&
+                            !( (int)(*curr) > 64 && (int)(*curr) < 71 ) &&
+                            !( (int)(*curr) > 96 && (int)(*curr) < 103)
+                        ){
+                            printf("Error: Bad formatting for hexadecimal number\n");
+                            printf("%c\n", *curr);
+                            printf("%d\n", (int)(*curr));
+                            return 1;
+                        }
+                        int halfbytecount = 0;
+                        while ( !( isspace(*curr) || *curr == '\\') ){
+
+                            decoded_str[ decoded_i++ ] = *(curr++);
+                            halfbytecount++;
+
+                            if (
+                            (   !( isdigit(*curr) ) &&
+                                !( ((int)(*curr) > 64 && (int)(*curr) < 71) ) &&
+                                !( ((int)(*curr) > 96 && (int)(*curr) < 103) ) &&
+                                !( isspace(*curr) ) &&
+                                !( *curr == '\\' )
+                            ) || halfbytecount > 16)
+
+                            {
+                                printf("Error: Bad formatting for hexadecimal number\n");
+                                printf("%c\n", *curr);
+                                return 1;
+                            }
+                            printf("curr is %c\n", *curr);
+
+
+                        }
+                        curr--;
+
+
+
                     }
 
                     else{
@@ -62,10 +159,15 @@ int string_decode( const char *es, char *s ){
                     }
 
                 }
-                else{
+                else if ((int)(*curr) > 31 && (int)(*curr) < 127){
                     decoded_str[ decoded_i++ ] = *curr;
                 }
+                else{
+                    printf("Error: Ascii value out of valid range\n");
+                    return 1;
+                }
                 curr++;
+                printf("current %c\n",*curr);
 
 
             }
@@ -87,6 +189,108 @@ int string_decode( const char *es, char *s ){
 
 
 int string_encode( const char *s, char *es ){
+
+    char encoded_str[MAX_STRING_LEN];
+    int encoded_i = 0;
+    memset(encoded_str, '\0', MAX_STRING_LEN);
+    char* curr = (char*)s;
+
+    if ( strlen(s) == 1 ){
+        printf("right spot\n");
+        encoded_str[ encoded_i++ ] = '\'';
+        if ( !( (int)(*curr) > 31 && (int)(*curr) < 127 ) ){
+            printf("Error: Bad character input\n");
+            return 1;
+        }
+        encoded_str[ encoded_i++ ] = *curr;
+
+        encoded_str[ encoded_i++ ] = '\'';
+
+        int len = sizeof(encoded_str);
+        strncpy(es, encoded_str, len);
+        printf("success: %s$\n", encoded_str);
+        return 0;
+
+
+
+    }
+
+
+    encoded_str[ encoded_i++ ] = '\"';
+
+    while ( *(curr) ){
+        char c = *curr;
+
+        if (c == '\n'){
+            encoded_str[ encoded_i++ ] = '\\';
+            encoded_str[ encoded_i++ ] = 'n';
+        }
+
+        else if ( c == '\t' ){
+            encoded_str[ encoded_i++ ] = '\\';
+            encoded_str[ encoded_i++ ] = 't';
+        }
+        else if ( c == '\v' ){
+            encoded_str[ encoded_i++ ] = '\\';
+            encoded_str[ encoded_i++ ] = 'v';
+        }
+        else if ( c == '\r' ){
+            encoded_str[ encoded_i++ ] = '\\';
+            encoded_str[ encoded_i++ ] = 'r';
+        }
+
+        else if ( c == '\\' ){
+            encoded_str[ encoded_i++ ] = '\\';
+            encoded_str[ encoded_i++ ] = '\\';
+        }
+
+        else if ( c == '\"' ){
+            encoded_str[ encoded_i++ ] = '\\';
+            encoded_str[ encoded_i++ ] = '\"';
+        }
+
+        else if ( c == '\'' ){
+            encoded_str[ encoded_i++ ] = '\\';
+            encoded_str[ encoded_i++ ] = '\'';
+        }
+        else if ( c == '0' ){
+            if ( *(curr+1) == 'x' ){
+                encoded_str[ encoded_i++ ] = '\\';
+                encoded_str[ encoded_i++ ] = '0';
+                encoded_str[ encoded_i++ ] = 'x';
+                curr++;
+                int halfbytecount = 0;
+                while( *(++curr) && !( isspace(*curr) ) && !( *curr == '\\' ) ){
+                    if (
+                    (   !( isdigit(*curr) ) &&
+                        !( ((int)(*curr) > 64 && (int)(*curr) < 71) ) &&
+                        !( ((int)(*curr) > 96 && (int)(*curr) < 103) ) &&
+                        !( isspace(*curr) )
+                    ) || halfbytecount > 16){
+                        printf("Error: Badly formatted hexadecimal");
+                        return 1;
+                    }
+                    encoded_str[ encoded_i++ ] = *curr;
+                    halfbytecount++;
+                }
+                curr--;
+            }
+        }
+        else if ( (int)(*curr) > 31 && (int)(*curr) < 127 ){
+            encoded_str[ encoded_i++ ] = *curr;
+        }
+        else{
+            printf("Error: Invalid characted added to string\n");
+            return 1;
+        }
+        curr++;
+
+    }
+    encoded_str[ encoded_i++ ] = '\"';
+    encoded_str[ encoded_i ] = '\0';
+    int len = sizeof(encoded_str);
+    strncpy(es, encoded_str, len);
+    printf("es success\n");
     return 0;
-    
+
 }
--- main.c
+++ main.c
@@ -3,98 +3,59 @@
 
 
 int main(int argc, char* argv[]){
-    printf("in main\n");
+
+    //encode test cases
     if ( argc == 3 && ( strcmp(argv[1],"--encode") == 0 ) ){
+
+        //initialize static arrays for file input
         char es[MAX_STRING_LEN];
-        char s[MAX_STRING_LEN];
-        char checkStr[MAX_STRING_LEN];
-        //char path2[17] = "test/encode/bad0";
+        char check_S[MAX_STRING_LEN];
+        char check_ES[MAX_STRING_LEN];
+        memset(check_S, '\0', MAX_STRING_LEN);
+        memset(check_ES, '\0', MAX_STRING_LEN);
+
+        //get filename from args
         FILE* fp = fopen( argv[2], "r" );
+
+        //get encoded string
         fgets(es, MAX_STRING_LEN, fp);
-        fgets(s, MAX_STRING_LEN, fp);
-        
+
+        //if there is not newline, could be invalid input altogether
         if ( es[ strlen(es) - 1  ] == '\n' ){
             es[ strlen(es) - 1  ] = '\0';
         }
         else{
             printf("Bminor String length exceeded\n");
-            printf("%s$\n%s$\n",es,s);
+            printf("%s$\n",es);
             return 1;
         }
-        if ( s[ strlen(s) - 1  ] == '\n' ) {
-            s[ strlen(s) - 1  ] = '\0';
+
+
+
+        if ( string_decode(es, check_S) == 1 ){
+            printf("Decoder Test failed\n");
+            printf("%s$ %s$\n",es,check_S);
+            return 1;
         }
         else{
-            printf("Bminor String length exceeded\n");
-            return 1;
+            printf("Decoded: %s\n", check_S);
         }
 
-        strcpy(checkStr, s);
-
-        if ( string_decode(es, s) == 1){
-            printf("Encoder Test failed: String decode\n");
+        if ( string_encode(check_S, check_ES) == 1 ){
+            printf("Encoder Test failed\n");
+            printf("%s$ %s$\n",check_S,check_ES);
             return 1;
-        }
-
-        else if (strcmp(s, checkStr) != 0){
-            printf("Encoder Test failed: Original string comparison\n");
-            return 1;
-        }
-        printf("success\n");
-        return 0;
-
-
-        /*
-        for (int i = 0; i < 10; i++){
-
-            path1[16] = (char)(48 + i);
-
-            FILE* fp = fopen( path1, "r" );
-
-            fgets(es, MAX_STRING_LEN, fp);
-            fgets(s, MAX_STRING_LEN, fp);
-
-            strcpy(checkStr, s);
-
-            if ( string_decode(es, s) == 1){
-                printf("Encoder Test %d failed: String decode\n", i);
-                fails++;
-            }
-
-            else if (strcmp(s, checkStr) != 0){
-                printf("Encoder Test %d failed: Original string comparison\n", i);
-                fails++;
-            }
-            memset(es, '\0', strlen(es));
-            memset(s, '\0', strlen(s));
-
         }
         
-        for (int i = 0; i < 10; i++){
-
-            path1[16] = (char)(48 + i);
-
-            FILE* fp = fopen( path1, "r" );
-
-            fgets(es, MAX_STRING_LEN, fp);
-            fgets(s, MAX_STRING_LEN, fp);
-
-            strcpy(checkStr, s);
-
-            if ( string_decode(es, s) == 1){
-                printf("Encoder Test %d failed: String decode\n", i);
-                fails++;
-            }
-
-            else if (strcmp(s, checkStr) != 0){
-                printf("Encoder Test %d failed: Original string comparison\n", i);
-                fails++;
-            }
-            memset(es, '\0', strlen(es));
-            memset(s, '\0', strlen(s));
-
+        else if (strcmp(check_ES, es) != 0){
+            printf("Final Encoded String not es\n");
+            printf("%s$ %s$\n", es, check_ES);
+            return 1;
         }
-        */
+
+
+        printf("success\n");
+        return 0;
 
 
     }
